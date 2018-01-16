8cd81326313d66f64171d3e343c363d11af9afd1
Scott Semyan <scottse@microsoft.com>
2018-01-16 11:07:59 +0000

    Get item values from SQL using MSI

--- SimpleCoreApi/Controllers/ValuesController.cs
+++ SimpleCoreApi/Controllers/ValuesController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 
 namespace SimpleCoreApi.Controllers
 {
@@ -14,8 +17,39 @@ namespace SimpleCoreApi.Controllers
         public IEnumerable<ItemInfo> Get()
         {
 			List<ItemInfo> stores = new List<ItemInfo>();
-			stores.Add(new ItemInfo { itemId = 1, itemName = "My Item" });
-			stores.Add(new ItemInfo { itemId = 2, itemName = "My Other Item" });
+			string connStr;
+			if (String.IsNullOrEmpty(Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME")))
+			{
+				// Running local
+				connStr = "Server=localhost;Initial Catalog=scottsedisdb;Integrated Security=SSPI;Persist Security Info=True;";
+			}
+			else
+			{
+				// Running in Azure
+				var azureServiceTokenProvider = new AzureServiceTokenProvider();
+				var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+				// TODO: handle errors if the web app cannot connect to the keyvault or get the secret. 
+				var secret = keyVaultClient.GetSecretAsync("https://scottsedisvault.vault.azure.net/secrets/ApiUserConnectionString-c19aed5e-4a64-41ce-9f0d-0b44300b223a/ba31031dc3094de0b6fa0907bd5a74c1").Result;
+				connStr = secret.Value;
+			}
+
+			using (var connection = new SqlConnection(connStr))
+			{
+				var command = new SqlCommand("select itemId, itemName from Items", connection);
+				connection.Open();
+				using (var reader = command.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						ItemInfo test = new ItemInfo
+						{
+							itemId = (int)reader["itemId"],
+							itemName = (string)reader["itemName"]
+						};
+						stores.Add(test);
+					}
+				}
+			}
 			return stores;
 		}
 
